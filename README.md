d6a5bfcb10b5ec486a78ea2735b339d5ef8db5c5
 Lê Hải Triều <trieulh@Les-Mac-mini.local>
2022-08-13 21:59:25 +0700

    Clean up Versions, update README and create debug apk

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Challenge
+
+This is a challenge project.
+
+## Stack:
+
+- **Architecture**: MVVM
+- **UI**: Compose, Compose Destionation for navigation
+- **Dependency Injection**: Hilt
+- **Unit Test**: Mockk, Turbine
+
+**Note:** You can find APK file for debug app in _/apk_ folder.
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,10 +4,8 @@ object Versions {
     const val targetSdk = compileSdk
 
     const val kotlin = "1.7.10"
-    const val dependencyCheckPlugin = "0.42.0"
     const val gradlePluginVersion = "7.2.1"
     const val hiltPlugin = "2.43.2"
-    const val googleServices = "4.3.13"
     const val coroutine = "1.6.4"
 
     const val composeViewModel = "2.5.1"
@@ -29,30 +27,20 @@ object Versions {
     const val composeCompiler = "1.3.0-rc01"
     const val composeUIDebug = "1.3.0-alpha02"
     const val composeNavigation = "2.5.1"
-    const val accompanist = "0.26.0-alpha"
 
     const val hiltCompose = "1.0.0"
 
-    const val firebaseBom = "30.3.1"
-
-    const val ktxSerialization = "1.3.3"
     const val mockk = "1.12.5"
 }
 
 object Deps {
 
     object Gradle {
-        const val googleServices = "com.google.gms:google-services:${Versions.googleServices}"
         const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
         const val kotlinSerialization =
             "org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}"
-        const val dependencyCheckPlugin = "com.github.ben-manes.versions"
         const val pluginVersion = "com.android.tools.build:gradle:${Versions.gradlePluginVersion}"
         const val hilt = "com.google.dagger:hilt-android-gradle-plugin:${Versions.hiltPlugin}"
-    }
-
-    object Android {
-        const val material = "com.google.android.material:material:${Versions.material}"
     }
 
     object AndroidX {
@@ -82,33 +70,12 @@ object Deps {
             "io.github.raamcosta.compose-destinations:animations-core:${Versions.composeDestinations}"
         const val ksp =
             "io.github.raamcosta.compose-destinations:ksp:${Versions.composeDestinations}"
-
-        const val accompanistMaterial =
-            "com.google.accompanist:accompanist-navigation-material:${Versions.accompanist}"
-        const val accompanistAnimation =
-            "com.google.accompanist:accompanist-navigation-animation:${Versions.accompanist}"
     }
 
     object DI {
         const val hiltAndroid = "com.google.dagger:hilt-android:${Versions.hiltPlugin}"
         const val hiltKapt = "com.google.dagger:hilt-android-compiler:${Versions.hiltPlugin}"
         const val hiltCompose = "androidx.hilt:hilt-navigation-compose:${Versions.hiltCompose}"
-    }
-
-    object Ksp {
-        const val api = "com.google.devtools.ksp:symbol-processing-api:${Versions.ksp}"
-    }
-
-    object KtxSerialization {
-        const val json =
-            "org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.ktxSerialization}"
-    }
-
-    object Firebase {
-        const val firebaseBom = "com.google.firebase:firebase-bom:${Versions.firebaseBom}"
-        const val firebaseAuth = "com.google.firebase:firebase-auth-ktx"
-        const val firebaseFirestore = "com.google.firebase:firebase-firestore-ktx"
-        const val coroutinePlayServices = "org.jetbrains.kotlinx:kotlinx-coroutines-play-services:${Versions.coroutine}"
     }
 
     object Test {
